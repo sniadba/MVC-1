c8b265987ff4798b1d520d3b3e1ac9643466e464
sniadba <163034374+sniadba@users.noreply.github.com>
2024-03-18 19:55:12 +0100

    Add files via upload

--- student.js
+++ student.js
@@ -1,18 +1,26 @@
-const FULLNAME = "Śniadach Bartosz";
-const STUDENT_ID = 46163;
-console.log(`My name is${FULLNAME}. Mystudent ID is ${STUDENT_ID}`);
+const fs = require('fs');
 
-function GetStudentFullname ()
-{
+function renderPage(studentData) {
+    const { code, name, lastName, gender, age, studyField } = studentData;
 
-    return FULLNAME;
+    const profileData = `
+        <h1>Student profile</h1>
+        <p>Number: ${code}</p>
+        <p>First Name: ${name}</p>
+        <p>Last Name: ${lastName}</p>
+        <p>Gender: ${gender}</p>
+        <p>Age: ${age}</p>
+        <p>Study Field: ${studyField}</p>
+    `;
+
+    // Zapis danych do pliku
+    fs.writeFile(`${code}.txt`, profileData, (err) => {
+        if (err) {
+            console.error('Error saving data to file:', err);
+        } else {
+            console.log('Data saved to file successfully!');
+        }
+    });
+
+    return profileData;
 }
-
-function GetStudentId ()
-{
-
-    return STUDENT_ID;
-}
-
-console.log(`My name is` + GetStudentFullname() `. Mystudent ID is ` + GetStudentId());
-
