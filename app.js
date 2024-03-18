c34541c25eb031ecc9d4128ad5c0925bfd4d1f71
sniadba <163034374+sniadba@users.noreply.github.com>
2024-03-18 19:35:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-module.export = {
-    GetStudentFullname,
-    GetStudentId,
-};
-
-console.log(`My name is` + GetStudentFullname() `. Mystudent ID is ` + GetStudentId());
-
-const http = require("http");
+const http = require('http');
 
 const PORT = 3000;
 
-function requestListener(request,response){
-    response.setHeader("Content-type", "text/html");
-    console.log(`serwer is running on ${PORT}` );
-    console.log(`My name is${FULLNAME}. Mystudent ID is ${STUDENT_ID}`);
+function requireListener(request, response){
+    console.log(`Server is running on ${PORT}`);
 }
+
+const server = http.createServer((request, response) => {
+if(url==="/"){
+    response.setHeader( 'Content-Type', 'text/html' );
+    response.write(`
+    <html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>home.js</title>
+    </head>
+    <body>
+        <div>home.js</div>
+    </body>
+    </html>
+    `);
+}
+
+if(url==="/Student"){
+    response.setHeader( 'Content-Type', 'text/html' );
+    response.write(`
+    <html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>student.js</title>
+    </head>
+    <body>
+        <div>student.js</div>
+    </body>
+    </html>
+    `);
+}
+});
+
+
+
+server.listen(PORT);
